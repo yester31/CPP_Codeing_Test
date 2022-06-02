fb5df37538d422758deaff543b0d911471893663
yhpark <yester31@naver.com>
2022-06-03 00:04:13 +0900

    update

--- lecture1/lecture1/ex065.cpp
+++ lecture1/lecture1/ex065.cpp
@@ -5,39 +5,61 @@
 using namespace std;
 
 // ¹Ì·ÎÅ½»ö(DFS)
-
-static vector<int> ch(30);
+static int n = 7;
+static vector<vector<int>> ch(n, vector<int>(n, 0));
 static int cnt = 0;
 
-static void dfs(int v, int n, vector<vector<int>>& map) {
-    if (v == n - 1) {
+static void dfs(int h, int w, vector<vector<int>>& map) {
+    if (n == h + 1 && n == w + 1) {
         cnt++;
     }
     else {
-        for (int i = 0; i < n; i++) {
-            if (map[v][i] == 1 && ch[i] == 0) {
-                ch[i] = 1;
-                dfs(i, n, map);
-                ch[i] = 0;
+        if (w + 1 < n) {// µ¿
+            if (map[h][w + 1] == 0 && ch[h][w + 1] == 0) {
+                ch[h][w + 1] = 1;
+                dfs(h, w + 1, map);
+                ch[h][w + 1] = 0;
+            }
+        }
+        if (w - 1 >= 0) {// ¼­
+            if (map[h][w - 1] == 0 && ch[h][w - 1] == 0) {
+                ch[h][w - 1] = 1;
+                dfs(h, w - 1, map);
+                ch[h][w - 1] = 0;
+            }
+        }
+        if (h + 1 < n) {// ³²
+            if (map[h + 1][w] == 0 && ch[h + 1][w] == 0) {
+                ch[h + 1][w] = 1;
+                dfs(h + 1, w, map);
+                ch[h + 1][w] = 0;
+            }
+        }
+        if (h - 1 >= 0) {// ºÏ
+            if (map[h - 1][w] == 0 && ch[h - 1][w] == 0) {
+                ch[h - 1][w] = 1;
+                dfs(h - 1, w, map);
+                ch[h - 1][w] = 0;
             }
         }
     }
 }
 
 void ex065() {
-    vector<vector<int>> map{{0, 0, 0, 0, 0, 0, 0},
+    vector<vector<int>> map{ {0, 0, 0, 0, 0, 0, 0},
                             {0, 1, 1, 1, 1, 1, 0},
                             {0, 0, 0, 1, 0, 0, 0},
                             {1, 1, 0, 1, 0, 1, 1},
                             {1, 1, 0, 0, 0, 0, 1},
                             {1, 1, 0, 1, 1, 0, 0},
                             {1, 0, 0, 0, 0, 0, 0} };
-
+    ch[0][0] = 1;
+    dfs(0, 0, map);
     cout << cnt << endl;
 }
 
-int main() {
-    ex065();
-    cout << endl << "done!" << endl;
-}
-//6
+//int main() {
+//    ex065();
+//    cout << endl << "done!" << endl;
+//}
+// 8
---
+++ lecture1/lecture1/ex066.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+using namespace std;
+
+// 경로 탐색(DFS : 인접리스트 방법)
+
+void ex066() {
+
+}
+
+int main() {
+    ex066();
+    cout << endl << "done!" << endl;
+}
+// 8
