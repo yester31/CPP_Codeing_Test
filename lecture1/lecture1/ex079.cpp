a9e68e4eeb68b5921735f6c4e32dce55302ea253
yhpark <yester31@naver.com>
2022-07-17 23:29:49 +0900

    update

--- lecture1/lecture1/ex079.cpp
+++ lecture1/lecture1/ex079.cpp
@@ -4,8 +4,19 @@
 
 using namespace std;
 
-// 원더랜드(Prim MST 알고리즘 : priority_queue 활용)
+// 원더랜드(Prim MST 알고리즘 (최소스패닝트리): priority_queue 활용)
 // 최소비용신장트리 
+static vector<int> ch(30);
+struct Edge2 {
+    int e, val;
+    Edge2(int a, int b) {
+        e = a;
+        val = b;
+    }
+    bool operator<(const Edge2 &b) const {
+        return val > b.val; // 내림차순(최소힙으로 리턴)
+    }
+};
 
 int ex079() {
     int n = 9; // 도시
@@ -22,14 +33,33 @@ int ex079() {
                                 {5, 7, 38},
                                 {7, 8, 35},
                                 {8, 9, 15} };
-
-    //cout << res << endl;
-
+    priority_queue<Edge2> Q;
+    vector<pair<int, int>> map[30]; // 가중치 인접리스트
+    for (int i = 0; i < m; i++) { // 무방향
+        map[inputs[i][0]].push_back(make_pair(inputs[i][1], inputs[i][2]));
+        map[inputs[i][1]].push_back(make_pair(inputs[i][0], inputs[i][2]));
+    }
+    int res = 0;
+    Q.push(Edge2(1, 0));
+    while (!Q.empty()) {
+        Edge2 tmp = Q.top();
+        Q.pop();
+        int v = tmp.e;
+        int cost = tmp.val;
+        if (ch[v] == 0) {
+            res += cost;
+            ch[v] = 1;
+            for (int i = 0; i < map[v].size(); i++) {
+                Q.push(Edge2(map[v][i].first, map[v][i].second));
+            }
+        }
+    }
+    cout << res << endl;
     return 0;
 }
 
-int main() {
-    ex079();
-    cout << endl << "done!" << endl;
-}
+//int main() {
+//    ex079();
+//    cout << endl << "done!" << endl;
+//}
 // 196
